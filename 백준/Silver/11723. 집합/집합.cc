7476d17ffe00e19917fbb0179c2d6e5a85978af5
송명석 <67058185+sms1875@users.noreply.github.com>
2024-08-13 14:43:06 +0900

    [Silver V] Title: 집합, Time: 692 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int num = 0;
+int bitmask = 0; // 비트 연산을 위한 변수
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -13,29 +13,36 @@ int main() {
 	cin >> n;
 	while (n--) {
 		string command;
-		int tmp;
+		int num;
 		cin >> command;
+
 		if (command == "add") {
-			cin >> tmp;
-			num = num |(1 << (tmp - 1));
+			cin >> num;
+			int bit_to_set = (1 << (num - 1)); // num 번째 비트를 1로 설정하기 위한 비트 마스크 계산
+			bitmask = bitmask | bit_to_set; // 비트 연산: OR 연산을 통해 num 번째 비트를 1로 설정
 		}
 		else if (command == "remove") {
-			cin >> tmp;
-			num = num & (~(1 << (tmp - 1)));
+			cin >> num;
+			int bit_to_clear = (1 << (num - 1)); // num 번째 비트를 0으로 설정하기 위한 비트 마스크 계산
+			bitmask = bitmask & (~bit_to_clear); // 비트 연산: AND 연산과 NOT 연산을 통해 num 번째 비트를 0으로 설정
 		}
 		else if (command == "check") {
-			cin >> tmp;
-			cout << ((num >> (tmp - 1)) & 1) << "\n";
+			cin >> num;
+			int bit_to_check = (1 << (num - 1)); // num 번째 비트를 확인하기 위한 비트 마스크 계산
+			int check_result = (bitmask & bit_to_check); // 비트 연산: AND 연산을 통해 num 번째 비트가 1인지 확인
+			int is_set = (check_result >> (num - 1)); // 비트 연산: 오른쪽으로 쉬프트하여 비트 값 추출 (0 또는 1)
+			cout << is_set << "\n"; // 비트 값 출력 (1 또는 0)
 		}
 		else if (command == "toggle") {
-			cin >> tmp;
-			num = num ^ (1 << (tmp - 1));
+			cin >> num;
+			int bit_to_toggle = (1 << (num - 1)); // num 번째 비트를 반전시키기 위한 비트 마스크 계산
+			bitmask = bitmask ^ bit_to_toggle; // 비트 연산: XOR 연산을 통해 num 번째 비트를 반전
 		}
 		else if (command == "all") {
-			num = 0xfffff;
+			bitmask = 0xFFFFF; // 모든 비트를 1로 설정 (20 비트의 모든 비트를 1로 설정)
 		}
 		else if (command == "empty") {
-			num = 0;
+			bitmask = 0; // 모든 비트를 0으로 설정
 		}
 	}
 	return 0;
