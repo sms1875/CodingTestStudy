7aed966359797c791ed8ad44f2e00d999259ed6e
송명석 <67058185+sms1875@users.noreply.github.com>
2024-09-10 11:57:22 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 248 ms, Memory: 4924 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve(const int &cur, vector<vector<int>> &prerequisites, vector<int> &dp) {
+	if (prerequisites[cur].size() == 0) {
+		dp[cur] = 1;
+	}
+	else {
+		int maxV = 0;
+		for (int i = 1; i <= prerequisites[cur].size(); i++)
+		{
+			int prerequisite = prerequisites[cur][i - 1];
+			if (dp[prerequisite] == -1) solve(prerequisites[cur][i], prerequisites, dp);
+			maxV = max(maxV, dp[prerequisite] + 1);
+		}
+		dp[cur] = maxV;
+	}
+}
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	vector<vector<int>> prerequisites(N+1);
+	vector<int> dp(N+1, -1);
+
+	for (int i = 1; i <= M; i++)
+	{
+		int A, B;
+		cin >> A >> B;
+		prerequisites[B].push_back(A);
+	}
+
+	for (int i = 1; i <= N ; i++)
+	{
+		solve(i, prerequisites, dp);
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		cout << dp[i] << " ";
+	}
+
+	return 0;
+}
